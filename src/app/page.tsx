e0b320ad8dc7a13818dc2b7e6fd67e54f8b78011
Alia Hassany <aliahassany217@gmail.com>
2025-10-30 15:40:19 +0000

    اين اضفتها لا اراءها

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -172,7 +172,7 @@ export default function Home() {
             </p>
           </div>
           <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-            {TESTIMONIALS.slice(0, 3).map(testimonial => {
+            {TESTIMONIALS.map(testimonial => {
                 const testimonialImage = PlaceHolderImages.find(p => p.id === testimonial.imageId);
                 return(
               <Card key={testimonial.id} className="text-center bg-background">
