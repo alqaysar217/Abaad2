318558d02168fd668aaec64ed877442727000b00
Alia Hassany <aliahassany217@gmail.com>
2025-10-30 14:18:29 +0000

    الان الازرار ظهرت اريد منك جعل الصور تظهر وتتحرك

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,8 +59,10 @@ export default function Home() {
     <div className="flex flex-col">
       {/* Hero Section */}
       <section className="relative h-[60vh] text-white">
-        <HeroSlider />
-        <div className="absolute inset-0 bg-primary/70 z-10"></div>
+        <div className="absolute inset-0 z-0">
+          <HeroSlider />
+        </div>
+        <div className="absolute inset-0 bg-black/50 z-10"></div>
         <div className="relative z-20 flex h-full items-center justify-center text-center">
             <div className="container mx-auto px-4">
             <AbaadLogo className="h-24 w-auto mx-auto mb-4" />
